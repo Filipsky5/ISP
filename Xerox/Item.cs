a07737e245115911cd4834adb8d1616542abcd45
Filip Jabłoński <filip.jablonski@oke.pl>
2016-04-26 16:18:22 +0200

    violates isp

---
+++ Xerox/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Xerox
+{
+	public class Item
+	{
+		int _numberOfItems;
+		public Item (int numberOfItems)
+		{
+			_numberOfItems = numberOfItems;
+		}
+		public int Count() {
+			return _numberOfItems;
+		}
+	}
+}
+
---
+++ Xerox/Machine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Xerox
+{
+	interface IMachine
+	{
+		public bool print(List<Item> item);
+		public bool staple(List<Item> item);
+		public bool fax(List<Item> item);
+	}
+	public class Machine
+	{
+		public Machine ()
+		{
+		}
+	}
+}
+
